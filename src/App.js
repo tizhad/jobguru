12bf42c9a55b8bcf42434c38dd351bbbe112c09e
tinarezanezhad <tinarezanezhaad@gmail.com>
2021-04-26 21:55:52 +0200

    initial setting for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Navbar from "./Components/Navigation/Navbar";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/about-us" component={AboutUs}></Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
